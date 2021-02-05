8076532b6f681ebaf12d75f529b7c355637df457
Bethelhem Teshibelay <se.bethelhem.teshibelay@gmail.com>
2021-02-05 17:32:51 +0300

    added the bricks on top

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,24 +29,24 @@ var paddleWidth = 75;
 var paddleX = (canvas.width- paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+var bricks = [];
+for (var c=0; c< brickColumnCount; c++){
+    bricks[c] = [];
+    for (var r = 0; r< brickRowCount;r++){
+        bricks[c][r]={x: 0, y: 0};
+    }
+}
+
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler,false);
-
-function drawBall(){
-    ctx.beginPath();
-    ctx.arc(x,y,ballRadius,0,Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
-function drawPaddle(){
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
 
 function keyDownHandler(e){
     if(e.key == "Right" || e.key == "ArrowRight"){
@@ -62,17 +62,60 @@ function keyUpHandler(e){
         leftPressed = false;
     }
 }
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x,y,ballRadius,0,Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+function drawPaddle(){
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+function drawBricks(){
+    for (var c = 0; c<brickColumnCount; c++){
+        for (var r = 0; r<brickRowCount; r++){
+            var brickX = (c*(brickWidth + brickPadding))+ brickOffsetLeft;
+            var brickY =(r*(brickHeight + brickPadding))+ brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(0,0,brickWidth,brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
 
 function draw(){
     ctx.clearRect(0,0,canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
     //make the ball bounce
     if(x+ dx > canvas.width-ballRadius || x + dx < ballRadius){
         dx = -dx;
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius){
+    if(y + dy < ballRadius){
         dy = -dy;
+    }else if(y + dy > canvas.height-ballRadius ){
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval); // Needed for Chrome to end game
+        }
+        /*
+        alert("GAME OVER!");
+        document.location.reload();
+        clearInterval(interval);*/
     }
     x+=dx;
     y+=dy;
@@ -90,7 +133,8 @@ function draw(){
     }
 }
 
-setInterval(draw,10);
+var interval = setInterval(draw,10);
 //changing the ball speed and changing its direction.
 //changing the color of the ball to a random color every time it hits the wall.
 //make the paddle move faster or slower or change is size
+//try changing the number of bricks in a row or a column or their positions
